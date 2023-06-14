8324236d50418566c40b6e765c81a3e4947a526d
huyenho <hohuyen17092000@gmail.com>
2023-06-14 10:53:09 +0700

    add git

--- react-to-enterprise/vite.config.ts
+++ react-to-enterprise/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  resolve: {cffß
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
