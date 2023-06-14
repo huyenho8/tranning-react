dc1ae020c87d05326fec15ee31de95f2bb8ffe71
huyenho <hohuyen17092000@gmail.com>
2023-06-14 16:03:55 +0700

    api layer

--- react-to-enterprise/vite.config.ts
+++ react-to-enterprise/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
