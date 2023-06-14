54c0085e56cd5c28aa62dd22364f61be8cba853e
huyenho <hohuyen17092000@gmail.com>
2023-06-14 11:02:53 +0700

    fix config

--- react-to-enterprise/vite.config.ts
+++ react-to-enterprise/vite.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import path from 'path'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+import handlebars from 'vite-plugin-handlebars'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  plugins: [
+    checker({
+      overlay: { initialIsOpen: false },
+      typescript: true,
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api-server/': '...',
+      '/authorization/': '...',
     },
   },
 })
