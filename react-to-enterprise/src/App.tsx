380f4f7ec4a8373a458713d9dd83c6a305a8ac5d
huyenho <hohuyen17092000@gmail.com>
2023-06-16 14:13:30 +0700

    context state provider

--- react-to-enterprise/src/App.tsx
+++ react-to-enterprise/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css'
-import AnimalExample from '@/components/AnimalExample'
+import GlobalSpinnerContextProvider from './context/GlobalSpinnerContext'
+import GlobalSpinnerExample from './components/GlobalSpinnerExample'
 function App() {
   return (
-    <div className="App mx-auto max-w-6xl text-center my-8">
-      <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <AnimalExample />
-    </div>
+    <GlobalSpinnerContextProvider>
+      <div className="App mx-auto max-w-6xl text-center my-8">
+        <h1 className="font-semibold text-2xl">
+          React - The Road To Enterprise
+        </h1>
+        <GlobalSpinnerExample />
+      </div>
+    </GlobalSpinnerContextProvider>
   )
 }
 export default App
