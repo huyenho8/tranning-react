60a031bb588c66bf4daf980115d5d4682daba4d2
huyenho <hohuyen17092000@gmail.com>
2023-06-16 15:34:02 +0700

    better state handing

--- react-to-enterprise/src/App.tsx
+++ react-to-enterprise/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css'
-import BusinessCardEditor from './components/BusinessCardEditor'
+import TasksBoard from './components/TasksBoard'
+import ShoppingList from './components/ShoppingList'
+import ShoppingListContextProvider from './context/ShoppingListContext'
+
 function App() {
   return (
-    <div className="App mx-auto max-w-6xl text-center my-8">
-      <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <BusinessCardEditor />
-    </div>
+    <ShoppingListContextProvider>
+      <div className="App mx-auto max-w-6xl text-center my-8">
+        <h1 className="font-semibold text-2xl">
+          React - The Road To Enterprise
+        </h1>
+        <TasksBoard />
+        <ShoppingList />
+      </div>
+    </ShoppingListContextProvider>
   )
 }
 export default App
