48eff5543bc4117f54c6febdcad20a0e494ed431
huyenho <hohuyen17092000@gmail.com>
2023-06-20 16:53:32 +0700

    reset store

--- react-to-enterprise/src/App.tsx
+++ react-to-enterprise/src/App.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import './App.css'
 import UsersManager from './components/UsersManager/UsersManager'
+import { useAppDispatch, useAppSelector } from './store/hooks'
+import { fetchUsers, resetUsers } from './components/UsersManager/usersSlice'
+import { resetStore } from './store'
+import { increment } from './store/counterSlice'
 
 function App() {
+  const dispatch = useAppDispatch()
+  const count = useAppSelector((state) => state.counter.count)
+
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <UsersManager />
+      <main>
+        <div className="space-x-4 my-8">
+          <button
+            className="shadow px-4 py-3 bg-blue-100"
+            onClick={() => dispatch(resetUsers())}
+          >
+            Reset users slice
+          </button>{' '}
+          <button
+            className="shadow px-4 py-3 bg-blue-100"
+            onClick={() => dispatch(resetStore())}
+          >
+            Reset store
+          </button>{' '}
+          <button
+            className="shadow px-4 py-3 bg-blue-100"
+            onClick={() => dispatch(fetchUsers())}
+          >
+            Fetch users
+          </button>
+          <button
+            className="shadow px-4 py-3 bg-blue-100"
+            onClick={() => dispatch(increment())}
+          >
+            Increment Counter {count}{' '}
+          </button>
+        </div>
+        <UsersManager />
+      </main>
     </div>
   )
 }
--- react-to-enterprise/src/store/index.ts
+++ react-to-enterprise/src/store/index.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createAction } from '@reduxjs/toolkit'
 import {
   persistStore,
   persistReducer,
@@ -12,9 +12,21 @@ import {
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import usersReducer from '@/components/UsersManager/usersSlice'
-const rootReducer = combineReducers({ users: usersReducer })
+import { counterReducer } from './counterSlice'
+export const resetStore = createAction('resetStore')
+const rootReducer = combineReducers({
+  users: usersReducer,
+  counter: counterReducer,
+})
+
+const appReducer: typeof rootReducer = (state, action) => {
+  if (action.type === resetStore.type) {
+    return rootReducer(undefined, action)
+  }
+  return rootReducer(state, action)
+}
 const persistConfig = { key: 'root', version: 1, storage }
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, appReducer)
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
