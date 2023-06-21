f6f61d04f66ee8b5c4094763e8afe0d8cabb3140
huyenho <hohuyen17092000@gmail.com>
2023-06-21 18:18:18 +0700

    zustand

--- react-to-enterprise/src/App.tsx
+++ react-to-enterprise/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
-import AnimalExample from '@/components/AnimalExample'
+import EventManager from './components/EventsManager/EventsManager'
+
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <AnimalExample />
+      <EventManager />
     </div>
   )
 }
+
 export default App
