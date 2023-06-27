b9d74bc8f0a18364e44763484d36fc70ee365976
huyenho <hohuyen17092000@gmail.com>
2023-06-27 14:18:58 +0700

    code spliting and lazy loading

--- react-to-enterprise/src/App.tsx
+++ react-to-enterprise/src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import './App.css'
-import AnimalExample from '@/components/AnimalExample'
+import { Link, Route, Routes } from 'react-router-dom'
+import Spinner from './components/Snipper'
+
+const About = lazy(() => import('./views/About'))
+const Contact = lazy(() => import('./views/Contact'))
+const Home = lazy(() => import('./views/Home'))
+
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <AnimalExample />
+      <div className="my-8">
+        <nav className="space-x-4">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link> <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+      <Suspense
+        fallback={
+          <div className="flex justify-center">
+            <Spinner show delay={500} />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
+
 export default App
---
+++ react-to-enterprise/src/views/Contact.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type ContactProps = {}
+
+const Contact = (props: ContactProps) => {
+  return <div>Contact</div>
+}
+
+export default Contact
