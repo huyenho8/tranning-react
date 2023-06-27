29698a8ff15d145324357dbe61f295acdb6e79be
huyenho <hohuyen17092000@gmail.com>
2023-06-27 20:59:59 +0700

    throttle and debounce

---
+++ react-to-enterprise/src/components/TrackCursor.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useMousePosition } from '@/hooks/useMousePosition'
+
+type TrackCursorProps = {}
+
+const TrackCursor = (props: TrackCursorProps) => {
+  const position = useMousePosition()
+
+  return (
+    <div>
+      Last tracked position - x: {position.x}, y: {position.y}
+    </div>
+  )
+}
+
+export default TrackCursor
