832aab2c49629dd9a19bfd76b9098457a40130bb
huyenho <hohuyen17092000@gmail.com>
2023-06-29 17:30:07 +0700

    testing api mocking

--- react-to-enterprise/src/App.tsx
+++ react-to-enterprise/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import NewsletterForm from './components/NewsletterForm'
 
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
       <div className="mt-8 max-w-[20rem] mx-auto"></div>
+      <NewsletterForm />
     </div>
   )
 }
