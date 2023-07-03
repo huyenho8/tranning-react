219e12560ce263250aa5d6fa0364da44535996ae
huyenho <hohuyen17092000@gmail.com>
2023-07-03 16:55:30 +0700

    ssg isr ssr with next.js

---
+++ react-to-enterprise/src/pages/admin/_middleware.tsx
@@ -0,0 +1,18 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const isLoggedIn = (req: NextRequest) => {
+  return true;
+};
+
+const isAdmin = (req: NextRequest) => {
+  return false;
+};
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (!isLoggedIn(req) && !isAdmin(req)) {
+    return NextResponse.redirect(`${req.nextUrl.origin}/forbidden`);
+  }
+
+  return NextResponse.next();
+}
